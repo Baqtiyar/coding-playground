594a670216995a81afb908fee96a2829455d8817
Mohammed Baqtiyar Ahmed Khan <84869024+Baqtiyar@users.noreply.github.com>
2024-10-29 20:53:09 -0500

    Add files via upload

---
+++ Day1/Data Structure.py
@@ -0,0 +1,13 @@
+
+# print the following
+# Hello world!
+# Hello, 'World!
+# Hello World (tab separated)
+# Hello
+# World
+
+print('Hello World!')
+print("Hello, 'World")
+print ('Hello\tWorld')
+print ('Hello\nWorld')
+
