7388649fb9ad599e29341a9f66557d950f6a8b3a
Mohammed Baqtiyar Ahmed Khan <84869024+Baqtiyar@users.noreply.github.com>
2024-11-03 23:46:44 -0600

    Update Data Structure.py

--- Day1/Data Structure.py
+++ Day1/Data Structure.py
@@ -11,3 +11,8 @@ print("Hello, 'World")
 print ('Hello\tWorld')
 print ('Hello\nWorld')
 
+#Q2. print the following 
+# Hello I'm going to perform simple linear regression and binary regression analysis 
+
+print (' Hello I'm going to perform simple linear regression and binary regression analysis')
+
