1cbeb2e7aec7a76c01e81cc549b2f009af52e985
Mohammed Baqtiyar Ahmed Khan <84869024+Baqtiyar@users.noreply.github.com>
2025-07-19 22:14:03 -0500

    Add files via upload

---
+++ program9.py
@@ -0,0 +1,33 @@
+# Print this pattern 
+ ####
+ ####
+ ####
+ ####
+
+# While loop 
+i=1
+while i<=4:
+    print("#", end=" ")
+    j=1
+    while j<=3:
+        print("#", end=" ")
+        j +=1
+    i +=1
+    print()
+# While loop
+i=1
+while i<=4:
+    j=2
+    while j<=7:
+        print("#", end=" ")
+        j +=1
+    i +=1
+    print()
+
+# For loop
+for i in range(1,5):
+    for j in range(2,8):
+        print("#", end =" ")
+    print()
+
+
